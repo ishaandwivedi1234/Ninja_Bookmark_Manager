b753b54d7bc5c32571560100ad400c0491831cf4
ishaandwivedi1234 <50709505+ishaandwivedi1234@users.noreply.github.com>
2020-02-06 23:47:38 +0530

    Update data_handler.php

--- Database/data_handler.php
+++ Database/data_handler.php
@@ -2,10 +2,10 @@
 $url = $_POST['link'];
 $title=$_POST['title'];
 $note =$_POST['note'];
-$db='bookmarkext';
+$db='id12251806_bookmarkext';
 $server ='localhost';
-$username = 'root';
-$password='';
+$username = 'id12251806_localhost';
+$password='********';
 $conn = new mysqli($server,$username,$password,$db);
 $sql="INSERT INTO bookmarks (url, title,note ) VALUES  ('$url','$title','$note')";
 if($conn->query($sql)===TRUE){
@@ -14,6 +14,7 @@ if($conn->query($sql)===TRUE){
 $server ='localhost';
 $db="internal_ass_boomark";
 $username='roo_folder_123_for_unique_id';
+
 
 
 ?>
