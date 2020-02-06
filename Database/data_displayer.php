5558c66dcd23d48bc91a5a2b00acff0aacfb9869
ishaandwivedi1234 <50709505+ishaandwivedi1234@users.noreply.github.com>
2020-02-06 23:45:53 +0530

    Update data_displayer.php

--- Database/data_displayer.php
+++ Database/data_displayer.php
@@ -1,19 +1,23 @@
 
 <html>
     <head>
-    <link  rel="stylesheet" type="text/css" href="style/style.css">
+    <link  rel="stylesheet" type="text/css" href="style.css">
     <link href="https://fonts.googleapis.com/css?family=Fugaz+One|Monoton&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Zhi+Mang+Xing&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Lobster&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Abel&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Press+Start+2P&display=swap" rel="stylesheet">
     </head>
     <body >
        <div class ="logo"> <img src="logo2.png" height ="95" width="100" align="middle"/>
-      <span class ="text">Here Is What Ninja Stored For You</span>
+      <span class ="text"><span id="h">H</span>ere Is What Ninja Stored For You...</span>
 </div><br><br><br><br><br> <br><br> <br><br> 
  
    <div class= "a">
     
   <form  method="POST" action="search.php">
-  <input type="text" placeholder="Search....." name="text" id="text">
-  <input type="submit" value="Search"  id="sbut" name="go/">
+  <input type="text" placeholder="Search....." name="text" id="text" autocomplete="off">
+  <input type="submit" value="Search"  id="sbut" name="go">
   <a href="data.json" id="export" download>
       Export 
   </a>
@@ -23,11 +27,12 @@
     
 
        <div class= "display">
+       <br><br>
        <?php
-$db='3302577_bookmark';
-$server ='fdb18.biz.nf';
-$username = '3302577_bookmark';
-$password='#golugolu02#';
+$db='id12251806_bookmarkext';
+$server ='localhost';
+$username = 'id12251806_localhost';
+$password='golugolu';
 $conn = new mysqli($server,$username,$password,$db);
 
 $fh = fopen('data.json', 'w');
@@ -53,7 +58,7 @@ $res=mysqli_query($conn,$sql);
 if(mysqli_num_rows($res)>0){
     while($row=mysqli_fetch_assoc($res)){
       echo "<div class ='show'>"."<div class ='title'> "."".$row['title']."</div>"."<div class ='url'>"
-        . "<a href= '".$row['url']."'>"."<div class ='visit'>Visit Site</div>"."</a></div>"."<div class ='note'>"."<span id='note_title'>Note</span> <br><br><div class ='note_display'> ".$row['note']."</div></div>"."</div>"."<br><br><br>";
+        . "<a href= '".$row['url']."' target='_blank'>"."<div class ='visit'>Visit Site</div>"."</a></div>"."<div class ='note'>"."<span id='note_title'>Note</span><br><span class ='note_display'> ".$row['note']."</span></div>"."</div>"."<br><br><br>";
     }
 }
 
